cc61b5097100aaf5174eb2df5ca5e87035abe1ed
bashlykov2005 <bashlykov2005@mail.ru>
2024-06-04 21:45:43 +0500

    main app, pages index, about

--- main/views.py
+++ main/views.py
@@ -3,13 +3,19 @@ from django.shortcuts import render
 
 
 def index(request):
-    context ={
-        "title": "Home",
-        "content": "Главная страница магазина - Home",
+    context = {
+        "title": "Home - Главная",
+        "content": "Магазин мебели ПАРУС",
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page" : "Текст о том, почему наш магазин такой класный, и какой у нас хороший товар"
+    }
+
+    return render(request, "main/about.html", context)
